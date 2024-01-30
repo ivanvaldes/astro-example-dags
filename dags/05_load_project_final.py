6ca114fd0ad53f5ffd20a2adff75bb3af69298ba
ivanvaldes <ivmigliore@gmail.com>
2024-01-29 22:32:51 -0500

    adding final project

---
+++ dags/05_load_project_final.py
@@ -0,0 +1,157 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models.connection import Connection
+from time import time_ns
+from datetime import datetime , timedelta
+from airflow.utils.dates import days_ago
+import os
+from pymongo import MongoClient
+from pandas import DataFrame
+from google.cloud import bigquery
+import pandas as pd
+
+default_args = {
+    'owner': 'Datapath',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+def get_connect_mongo():
+
+    CONNECTION_STRING ="mongodb+srv://atlas:T6.HYX68T8Wr6nT@cluster0.enioytp.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(CONNECTION_STRING)
+
+    return client
+
+def transform_date(text):
+    text = str(text)
+    d = text[0:10]
+    return d
+    
+def ingestar_orders_process():
+    print("Ingestar desde orders!")
+    client = bigquery.Client()
+    query_string = """
+    drop table if exists `my-first-project-411501.dep_raw.orders` ;
+    """
+    query_job = client.query(query_string)
+    rows = list(query_job.result())
+    print(rows)
+
+    dbconnect = get_connect_mongo()
+    dbname=dbconnect["retail_db"]
+    collection_name = dbname["orders"] 
+    orders = collection_name.find({})  
+    orders_df = DataFrame(orders)
+    dbconnect.close()
+
+    orders_df['_id'] = orders_df['_id'].astype(str)
+    orders_df['order_date']  = orders_df['order_date'].map(transform_date)
+    orders_df['order_date'] = pd.to_datetime(orders_df['order_date'], format='%Y-%m-%d').dt.date
+
+    orders_df.dtypes
+
+    orders_rows=len(orders_df)
+    if orders_rows>0 :
+        client = bigquery.Client()
+
+        table_id =  "my-first-project-411501.dep_raw.orders"
+        job_config = bigquery.LoadJobConfig(
+            schema=[
+                bigquery.SchemaField("_id", bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("order_id", bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("order_date", bigquery.enums.SqlTypeNames.DATE),
+                bigquery.SchemaField("order_customer_id", bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("order_status", bigquery.enums.SqlTypeNames.STRING),
+            ],
+            write_disposition="WRITE_TRUNCATE",
+        )
+
+
+        job = client.load_table_from_dataframe(
+            orders_df, table_id, job_config=job_config
+        )  
+        job.result()  # Wait for the job to complete.
+
+        table = client.get_table(table_id)  # Make an API request.
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema), table_id
+            )
+        )
+    else : 
+        print('alerta no hay registros en la tabla orders')
+
+
+def end_process():
+    print(" FIN DEL PROCESO!")
+
+def load_products():
+    print(f" INICIO LOAD PRODUCTS")
+    dbconnect = get_connect_mongo()
+    dbname=dbconnect["retail_db"]
+    collection_name = dbname["products"] 
+    products = collection_name.find({})
+    products_df = DataFrame(products)
+    dbconnect.close()
+    products_df['_id'] = products_df['_id'].astype(str)
+    products_df['product_description'] = products_df['product_description'].astype(str)
+    products_rows=len(products_df)
+    print(f" Se obtuvo  {products_rows}  Filas")
+    products_rows=len(products_df)
+    if products_rows>0 :
+        client = bigquery.Client(project='my-first-project-411501')
+        table_id =  "my-first-project-411501.dep_raw.products"
+        job_config = bigquery.LoadJobConfig(
+            schema=[
+                bigquery.SchemaField("_id", bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("product_id", bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("product_category_id", bigquery.enums.SqlTypeNames.INTEGER),
+                bigquery.SchemaField("product_name", bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("product_description", bigquery.enums.SqlTypeNames.STRING),
+                bigquery.SchemaField("product_price", bigquery.enums.SqlTypeNames.FLOAT),
+                bigquery.SchemaField("product_image", bigquery.enums.SqlTypeNames.STRING),
+            ],
+            write_disposition="WRITE_TRUNCATE",
+        )
+
+
+        job = client.load_table_from_dataframe(
+            products_df, table_id, job_config=job_config
+        )  
+        job.result()  # Wait for the job to complete.
+
+        table = client.get_table(table_id)  # Make an API request.
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema), table_id
+            )
+        )
+    else : 
+        print('alerta no hay registros en la tabla productos')
+
+with DAG(
+    dag_id="final_project",
+    schedule="20 04 * * *", 
+    start_date=days_ago(2), 
+    default_args=default_args
+) as dag:
+    step_ingestar_orders = PythonOperator(
+        task_id='step_ingestar_orders_id',
+        python_callable=ingestar_orders_process,
+        dag=dag
+    )
+    step_load = PythonOperator(
+        task_id='load_products_id',
+        python_callable=load_products,
+        dag=dag
+    )
+    step_end = PythonOperator(
+        task_id='step_end_id',
+        python_callable=end_process,
+        dag=dag
+    )
+    step_ingestar_orders
